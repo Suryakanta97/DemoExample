53d43689dd2986d258c5280589d3a3417d657dfd
Suryakanta97 <suryakanta97@github.com>
2021-07-31 11:05:07 +0530

    files Added

---
+++ algorithms-searching/src/test/java/com/surya/algorithms/textsearch/TextSearchAlgorithmsUnitTest.java
@@ -0,0 +1,25 @@
+package com.surya.algorithms.textsearch;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.surya.algorithms.textsearch.TextSearchAlgorithms;
+
+public class TextSearchAlgorithmsUnitTest {
+
+
+    @Test
+    public void testStringSearchAlgorithms() {
+        String text = "This is some nice text.";
+        String pattern = "some";
+
+        int realPosition = text.indexOf(pattern);
+        Assert.assertTrue(realPosition == TextSearchAlgorithms.simpleTextSearch(pattern.toCharArray(), text.toCharArray()));
+        Assert.assertTrue(realPosition == TextSearchAlgorithms.RabinKarpMethod(pattern.toCharArray(), text.toCharArray()));
+        Assert.assertTrue(realPosition == TextSearchAlgorithms.KnuthMorrisPrattSearch(pattern.toCharArray(), text.toCharArray()));
+        Assert.assertTrue(realPosition == TextSearchAlgorithms.BoyerMooreHorspoolSimpleSearch(pattern.toCharArray(), text.toCharArray()));
+        Assert.assertTrue(realPosition == TextSearchAlgorithms.BoyerMooreHorspoolSearch(pattern.toCharArray(), text.toCharArray()));
+    }
+
+}
